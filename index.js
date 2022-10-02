a666379ecf5a3d7f9a7b904d14517d7d8256977b
krishnendu basak <krisauz17@yahoo.com>
2022-10-02 19:14:31 +1100

    phase-1-arithmetic-lab si done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2 ;
+
+const random = Math.round(Math.random() * 1000) + 1;
+
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4 ;
+
+const max = Math.max(1,20,2,15,19);
